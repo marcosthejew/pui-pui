077b131efb321447319b747c5d94fb6a98e43b9e
Hernan <Hernan@localhost>
2013-05-16 19:03:22 -0530

    Se agregaron clases vac?as, para que se agregaran las carpetas vac?as al repositorio

---
+++ src/PuiPui-BackOffice/PruebasUnitarias/Clase/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuiPui_BackOffice.LogicaDeNegocios.Clase
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/PuiPui-BackOffice/PruebasUnitarias/Salon/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuiPui_BackOffice.LogicaDeNegocios.Salon
+{
+    public class Class1
+    {
+    }
+}
