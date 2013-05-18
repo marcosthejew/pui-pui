7675bc7bcbcc2a91afcafdce41d2cd64a3218b86
karemsegura <karemsegura@gmail.com>
2013-05-18 15:11:07 -0530

    Stored procedure para musculo

---
+++ scripts/Stored Procedures/SPMusculo.sql
@@ -0,0 +1,35 @@
+USE [puipuiDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[existeMusculo]
+@nombreMusculo nchar(50)
+as
+
+BEGIN
+	set nocount on
+		
+	SELECT M.id_musculo
+	FROM Musculo AS M
+	where M.nombre = @nombreMusculo		
+END;
+
+GO
+
+USE [puipuiDB]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[agregarMusculo]
+@nombreMusculo nchar(50)
+as
+
+BEGIN
+	INSERT INTO Musculo (nombre) VALUES (@nombreMusculo);	
+END;
+
+GO
