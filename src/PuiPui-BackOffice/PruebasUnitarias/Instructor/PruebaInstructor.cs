f061cc5312e7dbe31e3d182055de10c6df83bfc0
Patricia <Patricia@localhost>
2013-05-17 21:10:25 -0530

    eliminando archivos Class1 y renombrandolos con  su correspondiente

--- src/PuiPui-BackOffice/PruebasUnitarias/Instructor/PruebaInstructor.cs
+++ src/PuiPui-BackOffice/PruebasUnitarias/Instructor/PruebaInstructor.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace PuiPui_BackOffice.LogicaDeNegocios.Rutina
+namespace PuiPui_BackOffice.PruebasUnitarias.Instructor
 {
-    public class Class1
+    public class PruebaInstructor
     {
     }
 }
--- src/PuiPui-BackOffice/PruebasUnitarias/Rutina/PruebaRutina.cs
+++ src/PuiPui-BackOffice/PruebasUnitarias/Rutina/PruebaRutina.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace PuiPui_BackOffice.LogicaDeNegocios.Instructor
+namespace PuiPui_BackOffice.PruebasUnitarias.Rutina
 {
-    public class Class1
+    public class PruebaRutina
     {
     }
 }
