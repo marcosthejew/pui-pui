4a208b54d1f527c644efa8779a496ac922a14e5f
CarlosCede?o <CarlosCede?o@localhost>
2013-05-19 14:06:43 -0530

    Agregando inserts de Salon y Clase

---
+++ scripts/Inserts/InsertsClaseySalon.sql
@@ -0,0 +1,19 @@
+USE [puipuiDB]
+INSERT INTO Clase VALUES ('Spinning','Es un ejercicio aeróbico con bicicletas',1 );
+INSERT INTO Clase VALUES ('DragonFight',' Es un sistema de entrenamiento diseñado con la base del karate contemporáneo',1 );
+INSERT INTO Clase VALUES ('TaeBo','Es una rutina de ejercicios principalmente asociados al boxeo y al Taekwondo',1 );
+INSERT INTO Clase VALUES ('Bailoterapia','Ejercicio al ritmo de la música que permite a los participantes divertirse y tonificarse',1 );
+INSERT INTO Clase VALUES ('Abdominales','Ejercicio para tonificar el área media',1 );
+INSERT INTO Clase VALUES ('Fit Combat','Rutina de fitness que involucra el combate cuerpo a cuerpo',1 );
+INSERT INTO Clase VALUES ('Glutab','Ejercicio para las chicas donde se ejercitan los gluteos y el abdomen simultaneamente',1 );
+INSERT INTO Clase VALUES ('TRX','Ejercicio con cuerdas para tonificar los musculos',1 );
+INSERT INTO Clase VALUES ('Pilates','Se centra en el desarrollo de los músculos internos para mantener el equilibrio corporal',1 );
+INSERT INTO Clase VALUES ('Yoga','Ejercicio que permite estirar el cuerpo y lograr paz interna',1 );
+
+INSERT INTO SALON VALUES (1,"Segundo Piso-Ala Este",30);
+INSERT INTO SALON VALUES (2,"Primer Piso-Ala 0ste",40);
+INSERT INTO SALON VALUES (3,"Primer Piso",100);
+INSERT INTO SALON VALUES (4,"Segundo Piso-Ala Norte",100);
+INSERT INTO SALON VALUES (5,"Tercer Piso",100);
+INSERT INTO SALON VALUES (6,"Primer Piso-Ala Este",50);
+INSERT INTO SALON VALUES (7,"Primer Piso-Ala Oeste",20);
