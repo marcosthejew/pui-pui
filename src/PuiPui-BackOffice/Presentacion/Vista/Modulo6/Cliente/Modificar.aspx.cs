709005e129bf2c494ed9871fefe51f466d84c68e
NUBIELIS <NUBIELIS@NUBI-PEREIRA>
2013-05-21 12:03:10 -0530

    Agregando consultas por parametros al modificar

--- src/PuiPui-BackOffice/Presentacion/Vista/Modulo6/Cliente/Modificar.aspx.cs
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo6/Cliente/Modificar.aspx.cs
@@ -24,10 +24,76 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo6.Cliente
             if (!IsPostBack)
             {
                 cargarTabla();
+
+                //Habilito si quiero buscar por ci o nombre, pero no ambos a la vez
+                if (cedulaRadioButton.Checked == true)
+                {
+                    NombreTextBox.Enabled = false;
+                    CiConsTextBox.Enabled = true;
+                }
+                if (cargoRadioButton.Checked == true)
+                {
+                    CiConsTextBox.Enabled = false;
+                    NombreTextBox.Enabled = true;
+                }
             }
             else
                 //Exito.Visible = false;
                 GridModificar.Visible = true;
+        }
+
+        #region Buscar por Cedula o Cargo
+        protected void buscarCiCargoButton_Click(object sender, EventArgs e)
+        {
+            if (cedulaRadioButton.Checked && !CiConsTextBox.Text.Equals(""))
+            {
+                LogicaPersona logicaPersona = new LogicaPersona();
+                GridModificar.DataSource = logicaPersona.ConsultarPorCedula(CiConsTextBox.Text);
+                GridModificar.DataBind();
+            }
+            if (cargoRadioButton.Checked && !NombreTextBox.Text.Equals(""))
+            {
+                LogicaPersona logicaPersona = new LogicaPersona();
+                if (!NombreTextBox.Text.ToString().Contains("Elija un Cargo"))
+                {
+                    GridModificar.DataSource = logicaPersona.ConsultarPorNombre(NombreTextBox.Text);
+                    GridModificar.DataBind();
+                }
+                else
+                    cargarTabla();
+            }
+        }
+        #endregion Buscar por Cedula o Cargo
+
+        protected void cedulaRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cedulaRadioButton.Checked)
+            {
+                CiConsTextBox.Enabled = false;
+                NombreTextBox.Enabled = true;
+                CiConsTextBox.Text = "";
+            }
+            else if (cedulaRadioButton.Checked)
+            {
+                CiConsTextBox.Enabled = true;
+                NombreTextBox.Enabled = false;
+            }
+        }
+
+        protected void cargoRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cargoRadioButton.Checked)
+            {
+                NombreTextBox.Enabled = true;
+                CiConsTextBox.Enabled = false;
+                CiConsTextBox.Text = "";
+            }
+            else if (!cargoRadioButton.Checked)
+            {
+                NombreTextBox.Enabled = false;
+                CiConsTextBox.Enabled = true;
+                //prueba
+            }
         }
 
         protected void GridModificar_SelectedIndexChanged(object sender, EventArgs e)
@@ -54,14 +120,15 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo6.Cliente
 
             DataTable table = new DataTable();
             table.Columns.Add("idPersona", typeof(string));
-            table.Columns.Add("PrimerNombre", typeof(string));
-            table.Columns.Add("PrimerApellido", typeof(string));
-            table.Columns.Add("FechaRegistro", typeof(string));
+            table.Columns.Add("cedulaPersona", typeof(string));
+            table.Columns.Add("nombrePersona1", typeof(string));
+            table.Columns.Add("apellidoPersona1", typeof(string));
+            table.Columns.Add("fechaIngresoPersona", typeof(string));
 
             foreach (Persona persona in listaPersona)
             {
-                table.Rows.Add(persona.IdPersona, persona.NombrePersona1,
-                persona.ApellidoPersona1, persona.FechaIngresoPersona.ToString("dd-MM-yyyy"));
+                table.Rows.Add(persona.IdPersona, persona.CedulaPersona, persona.NombrePersona1,
+               persona.ApellidoPersona1, persona.FechaIngresoPersona.ToString("dd-MM-yyyy"));
             }
 
             GridModificar.DataSource = table;
