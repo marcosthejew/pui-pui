6160d44abfcebb604e518765f0975578fe29a425
Patricia <Patricia@localhost>
2013-05-21 13:37:33 -0530

    Consultar y Modificar cliente del Front Office

--- src/PuiPui-FrontOffice/AccesoDeDatos/SQLServer/SQLServerPersona.cs
+++ src/PuiPui-FrontOffice/AccesoDeDatos/SQLServer/SQLServerPersona.cs
@@ -365,5 +365,65 @@ namespace PuiPui_FrontOffice.AccesoDeDatos.SQLServer
             return false;
         }
         #endregion
+
+        public Persona ConsultarPersonaPorLogin(string loginPersona)
+        {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["ConnPuiPui"].ToString();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            Persona objetoPersona = new Persona();
+
+            try
+            {
+                conexion = new SqlConnection(cadenaConexion);
+                conexion.Open();
+                cmd = new SqlCommand("[dbo].[consultarPersonaPorLogin]", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@loginPersona", loginPersona);
+                dr = cmd.ExecuteReader();
+
+                //Se recorre cada row
+                while (dr.Read())
+                {
+
+                    objetoPersona = new Persona();
+
+                    objetoPersona.IdPersona = Convert.ToInt32(dr.GetValue(0));
+                    objetoPersona.CedulaPersona = Convert.ToInt32(dr.GetValue(1));
+                    objetoPersona.NombrePersona1 = dr.GetValue(2).ToString();
+                    objetoPersona.NombrePersona2 = dr.GetValue(3).ToString();
+                    objetoPersona.ApellidoPersona1 = dr.GetValue(4).ToString();
+                    objetoPersona.ApellidoPersona2 = dr.GetValue(5).ToString();
+                    objetoPersona.GeneroPersona = dr.GetValue(6).ToString();
+                    objetoPersona.FechaNacimientoPersona = Convert.ToDateTime(dr.GetValue(7));
+                    objetoPersona.FechaIngresoPersona = Convert.ToDateTime(dr.GetValue(8));
+                    objetoPersona.CiudadPersona = dr.GetValue(9).ToString();
+                    objetoPersona.DireccionPersona = dr.GetValue(10).ToString();
+                    objetoPersona.TelefonoLocalPersona = dr.GetValue(11).ToString();
+                    objetoPersona.TelefonoCelularPersona = dr.GetValue(12).ToString();
+                    objetoPersona.CorreoPersona = dr.GetValue(13).ToString();
+                    objetoPersona.ContactoNombrePersona = dr.GetValue(14).ToString();
+                    objetoPersona.ContactoTelefonoPersona = dr.GetValue(13).ToString();
+                    objetoPersona.EstadoPersona = dr.GetValue(16).ToString();
+                    objetoPersona.LoginPersona = dr.GetValue(17).ToString();
+                    objetoPersona.PasswordPersona = dr.GetValue(18).ToString();
+                    objetoPersona.TipoPersona = dr.GetValue(19).ToString();
+                }
+
+                db.CerrarConexion();
+
+            }
+            catch (SqlException error)
+            {
+                //En caso de que se viole alguna restriccion sobre la BD
+                throw (new ExcepcionConexion(("Error: " + error.Message), error));
+            }
+            finally
+            {
+                db.CerrarConexion();
+            }
+            return objetoPersona;
+        }
     }
 }
