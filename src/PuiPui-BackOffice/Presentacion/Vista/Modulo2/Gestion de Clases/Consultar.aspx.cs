ca9c1c183e7f22dac33a2367cdff90b8fc9d8c74
Yeimy <Yeimy@localhost>
2013-05-22 17:12:08 -0530

    Modificaciones Clases

--- src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion de Clases/Consultar.aspx.cs
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion de Clases/Consultar.aspx.cs
@@ -12,48 +12,71 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
 {
     public partial class Consultar : System.Web.UI.Page
     { 
-        private List<Clase> _idClase;
-        private String _nombreClase ;
-        private int _estatusClase;
-        private  LogicaClase _logicaClase;
+        private List<Clase> _listaClase;       
+        private  LogicaClase _logicaClase= new LogicaClase();
         private int seleccionCheck;
-
-        public Consultar()
-        {
-            
-            //this._estatusClase = Int32.Parse(DropDownListEstatusClase.SelectedValue);
-            this._logicaClase = new LogicaClase();
-        }
-   
+        private int Estatus;
+       
         protected void Page_Load(object sender, EventArgs e)
         {
-                       
-            
+           
+            try
+            {
+                _listaClase = _logicaClase.ObtenerClases();
+                if (!IsPostBack)
+                {
+                    GridConsultar.Visible = false;
+                    cargarTabla();
+                }
+                else
+                {
+                    GridConsultar.Visible = true;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                
+               Response.Redirect("../../Home/Modulo2/Gestion_de_Clases/Consultar.aspx");
+            }
         }
 
         protected void GridConsultar_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+      /*  int seleccion = GridConsultar.SelectedIndex;
+           Clase clases = _listaClase[seleccion];
+           Session["No"] = clases;
+           Response.Redirect("DetalleConsultar.aspx");*/
         }
 
         protected void GridConsultar_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-                GridConsultar.PageIndex = e.NewPageIndex;
-                
+            GridConsultar.PageIndex = e.NewPageIndex;
+            //cargarTabla();
 
-                switch (seleccionCheck)
-                {
-                    case 1:
 
-                        GridConsultar.DataSource = _logicaClase.ConsultarClase();
-                        GridConsultar.DataBind();
-                        break;
-                    case 2:
-                        this._nombreClase = nombreClase.Text;
-                        GridConsultar.DataSource = _logicaClase.ConsultarClasesNombre(_nombreClase);
-                        GridConsultar.DataBind();
+            switch (seleccionCheck)
+            {
+                case 1:
+
+                    GridConsultar.DataSource = _logicaClase.ConsultarClase();
+                    GridConsultar.DataBind();
+
                     break;
-                }
+
+                case 2:
+                    GridConsultar.DataSource = _logicaClase.ConsultarClasesNombre(nombreClase.Text);
+                    GridConsultar.DataBind();
+
+                    break;
+
+                case 3:
+
+                    GridConsultar.DataSource = _logicaClase.ConsultarClaseStatus(DropDownListEstatusClase.SelectedIndex);
+                    GridConsultar.DataBind();
+
+                    break;
+
+            }
         }
 
         protected void GridConsultar_RowCommand(Object sender, GridViewCommandEventArgs e)
@@ -74,11 +97,11 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
 
             }
 
-
         }
 
         protected void botonBuscarClase_Click(object sender, EventArgs e)
         {
+            
             switch (seleccionCheck)
             {
                 case 1: 
@@ -89,47 +112,73 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
                 break;
 
                 case 2:
-                this._nombreClase = nombreClase.Text;
-                GridConsultar.DataSource = _logicaClase.ConsultarClasesNombre(_nombreClase);
+                GridConsultar.DataSource = _logicaClase.ConsultarClasesNombre(nombreClase.Text);                
                 GridConsultar.DataBind();
 
                 break;
 
                 case 3:
-                    
-                GridConsultar.DataSource = _logicaClase.ConsultarClase();
+                if (DropDownListEstatusClase.SelectedItem.ToString().Equals("Activa"))
+                {
+                     Estatus = 1;
+                }
+                else 
+                {
+                     Estatus = 0;
+                }
+                
+                GridConsultar.DataSource = _logicaClase.ConsultarClaseStatus(Estatus);                
                 GridConsultar.DataBind();
+
                 break;
 
             }
-
-
-            
-
-
-        }
-
-        protected void consultaClasePorEstatus_CheckedChanged(object sender, EventArgs e)
-        {
-            seleccionCheck = 2;
-        }
-
-        protected void consultaClasePorNombres_CheckedChanged1(object sender, EventArgs e)
-        {
-            seleccionCheck = 3;
-
-        }
-
-        protected void RadioButtonConsultaCompleta_CheckedChanged(object sender, EventArgs e)
-        {
-            seleccionCheck = 1;
-        }
+        }       
 
         protected void RadioButtonConsultaCompleta_CheckedChanged1(object sender, EventArgs e)
         {
             seleccionCheck = 1;
         }
 
-          
+        protected void consultaClasePorNombres_CheckedChanged(object sender, EventArgs e)
+        {
+            seleccionCheck = 2;
+        }
+
+        protected void consultaClasePorEstatus_CheckedChanged1(object sender, EventArgs e)
+        {
+            seleccionCheck = 3;
+        }
+
+        public void cargarTabla()
+        {
+
+            String stat = null;
+            DataTable miTabla = new DataTable();
+            miTabla.Columns.Add("No.", typeof(string));
+            miTabla.Columns.Add("Nombre", typeof(string));
+            miTabla.Columns.Add("Estatus", typeof(string));
+
+            foreach (Clase clase in _listaClase)
+            {
+                if (clase.Status == 1)
+                {
+                    stat = "Activa";
+                }
+                else
+                {
+                    stat = "Inactiva";
+                }
+                miTabla.Rows.Add(clase.IdClase, clase.Nombre, stat);
+            }
+            GridConsultar.DataSource = miTabla;
+            GridConsultar.DataBind();
+        }
+
+        protected void DropDownListEstatusClase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+         
+        }
     }
 }
