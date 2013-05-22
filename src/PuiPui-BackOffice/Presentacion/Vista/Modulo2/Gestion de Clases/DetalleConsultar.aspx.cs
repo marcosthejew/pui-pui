02ea5eeff0c189ac6499af4a7ad73363ea863bd4
Yeimy <Yeimy@localhost>
2013-05-22 18:19:17 -0530

    DEtalle-consult clase

---
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion de Clases/DetalleConsultar.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using PuiPui_BackOffice.Entidades.Clase;
+using PuiPui_BackOffice.LogicaDeNegocios.LogicaClase;
+
+namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String id = Convert.ToString((Request.QueryString["id"] != null) ? Request.QueryString["id"] : "");
+            String nombre = Convert.ToString((Request.QueryString["nombre"] != null) ? Request.QueryString["nombre"] : "");
+            String estatus = Convert.ToString((Request.QueryString["estatus"] != null) ? Request.QueryString["estatus"] : "");
+        }
+    }
+}
