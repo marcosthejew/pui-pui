392c995ae3ed6bcb36124d0ae73fd9991fdfc837
jon gon1 <jon.gon1@gmail.com>
2013-05-22 11:47:57 -0530

    Backed out changeset: 74ee608ba84d

--- src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion de Clases/Consultar.aspx.cs
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion de Clases/Consultar.aspx.cs
@@ -12,12 +12,11 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
 {
     public partial class Consultar : System.Web.UI.Page
     { 
-        private List<Clase> _listaClase;
+        private List<Clase> _idClase;
         private String _nombreClase ;
         private int _estatusClase;
         private  LogicaClase _logicaClase;
         private int seleccionCheck;
-       
 
         public Consultar()
         {
@@ -28,58 +27,33 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
    
         protected void Page_Load(object sender, EventArgs e)
         {
-           
-            try
-            {
-                _listaClase = _logicaClase.ObtenerClases();
-                if (!IsPostBack)
-                {
-                    GridConsultar.Visible = false;
-                    cargarTabla();
-                }
-                else
-                {
-                    GridConsultar.Visible = true;
-                }
-            }
-            catch (NullReferenceException)
-            {
-                
-               Response.Redirect("../../Home/Modulo2/Gestion_de_Clases/Consultar.aspx");
-            }
-           
+                       
             
         }
 
         protected void GridConsultar_SelectedIndexChanged(object sender, EventArgs e)
         {
-        int seleccion = GridConsultar.SelectedIndex;
-           Clase clases = _listaClase[seleccion];
-           Session["No"] = clases;
-           Response.Redirect("DetalleConsultar.aspx");
+
         }
 
         protected void GridConsultar_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-            GridConsultar.PageIndex = e.NewPageIndex;
-            cargarTabla();
+                GridConsultar.PageIndex = e.NewPageIndex;
                 
 
-                /*switch (seleccionCheck)
+                switch (seleccionCheck)
                 {
                     case 1:
 
                         GridConsultar.DataSource = _logicaClase.ConsultarClase();
                         GridConsultar.DataBind();
                         break;
-
                     case 2:
-
                         this._nombreClase = nombreClase.Text;
                         GridConsultar.DataSource = _logicaClase.ConsultarClasesNombre(_nombreClase);
                         GridConsultar.DataBind();
-                        break;
-                }*/
+                    break;
+                }
         }
 
         protected void GridConsultar_RowCommand(Object sender, GridViewCommandEventArgs e)
@@ -156,32 +130,6 @@ namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_de_Clases
             seleccionCheck = 1;
         }
 
-        public void cargarTabla()
-        {
-
-            String stat = null;
-            DataTable miTabla = new DataTable();
-            miTabla.Columns.Add("No.", typeof(string));
-            miTabla.Columns.Add("Nombre", typeof(string));
-            miTabla.Columns.Add("Estatus", typeof(string));
-
-            foreach (Clase clase in _listaClase)
-            {
-                if (clase.Status == 1)
-                {
-                    stat = "Activa";
-                }
-                else
-                {
-                    stat = "Inactiva";
-                }
-                miTabla.Rows.Add(clase.IdClase, clase.Nombre, stat);
-            }
-            GridConsultar.DataSource = miTabla;
-            GridConsultar.DataBind();
-        }
-
-
-
+          
     }
 }
