e0ab08fed5b62d5c0cc0f862055300ae8298499c
Naudy <Naudy@localhost>
2013-05-23 15:54:23 -0530

    agregar

--- src/PuiPui-FrontOffice/Presentacion/Vista/Modulo3/ReservarClase/Agregar.aspx.cs
+++ src/PuiPui-FrontOffice/Presentacion/Vista/Modulo3/ReservarClase/Agregar.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PuiPui_FrontOffice.Entidades.ClaseSalon;
+using PuiPui_FrontOffice.LogicaDeNegocios.EvaluacionClases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,40 @@ namespace PuiPui_FrontOffice.Presentacion.Vista.Modulo3.ReservarClase
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Panel1.Visible = false;
+            EvaluacionClase evaluacionClase = new EvaluacionClase();
+            List<ClaseSalon> listaClases =  evaluacionClase.consultarEvaluacionClases();
+
+            foreach (ClaseSalon objetoC in listaClases)
+            {
+                DropDownListClases.Items.Add("Clase: "+objetoC.FkIdClase.Nombre + " del Instructor: " + objetoC.FkIdInstructor.NombrePersona1 + " " + objetoC.FkIdInstructor.ApellidoPersona1);
+            }
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DropDownListClases.Visible = false;
+            ButtonEvaluar.Visible = false;
+            Label1.Text = "Por favor llenar la plantilla de evaluacion";
+            Panel1.Visible = true;
+
+            
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            int text1 = Convert.ToInt16(TextBox2.Text.ToString());
+            int text2 = Convert.ToInt16(TextBox2.Text.ToString());
+            int text3 = Convert.ToInt16(TextBox3.Text.ToString());
+            int text4 = Convert.ToInt16(TextBox4.Text.ToString());
+            int text5 = Convert.ToInt16(TextBox5.Text.ToString());
+
+            int suma = (text1 + text2 + text3 + text4 + text5)/5;
+
+            String obs = TextBox6.Text.ToString();
+
+            Label1.Text = "Su evaluacion promedio fue de :" + suma.ToString() + "con las siguientes observaciones: " + obs;
 
         }
     }
