d5897439d22d4de02536b1146a8f6a11aaf376ec
Gustavo <Gustavo@localhost>
2013-05-22 22:50:55 -0530

    CRUD Evaluacion Instructores

---
+++ scripts/Inserts/InsertsEvaluacion.sql
@@ -0,0 +1,13 @@
+USE [puipuiDB]
+INSERT INTO Evaluacion VALUES (getdate(),3,NULL,NULL,1);
+INSERT INTO Evaluacion VALUES (getdate(),5,NULL,NULL,2);
+INSERT INTO Evaluacion VALUES (getdate(),2,NULL,NULL,1);
+INSERT INTO Evaluacion VALUES (getdate(),1,NULL,NULL,2);
+INSERT INTO Evaluacion VALUES (getdate(),4,NULL,NULL,1);
+INSERT INTO Evaluacion VALUES (getdate(),3,NULL,NULL,3);
+INSERT INTO Evaluacion VALUES (getdate(),3,NULL,NULL,3);
+INSERT INTO Evaluacion VALUES (getdate(),5,NULL,NULL,2);
+INSERT INTO Evaluacion VALUES (getdate(),1,NULL,NULL,3);
+
+
+--select CONVERT (char(10), getdate(), 103)
--- src/PuiPui-FrontOffice/Presentacion/Vista/Modulo4/EvaluarInstructor/Consultar.aspx.cs
+++ src/PuiPui-FrontOffice/Presentacion/Vista/Modulo4/EvaluarInstructor/Consultar.aspx.cs
@@ -4,14 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PuiPui_FrontOffice.LogicaDeNegocios.EvaluacionInstructor;
+using PuiPui_FrontOffice.Entidades.EEvaluacionInstructor;
 
 namespace PuiPui_FrontOffice.Presentacion.Vista.Modulo4.EvaluarInstructor
 {
     public partial class Consultar : System.Web.UI.Page
     {
+
+        List<EEvaluacionInstructor> _listaEvaluacion;
+
+        #region Page Load
         protected void Page_Load(object sender, EventArgs e)
         {
+            LogicaEvaluacionInstructor evaluacion = new LogicaEvaluacionInstructor();
+            _listaEvaluacion = evaluacion.ConsultarEvaluacion();
+
+            if (!IsPostBack)
+            {
+                llenarGridViewEvaluaciones();
+            }
 
         }
+        #endregion
+
+        #region Llenar GridView Evaluaciones
+        protected void llenarGridViewEvaluaciones()
+        {
+
+            LogicaEvaluacionInstructor evaluaciones = new LogicaEvaluacionInstructor();
+            EvaluacionesTodasGrid.DataSource = evaluaciones.ConsultarEvaluacion();
+            EvaluacionesTodasGrid.DataBind();
+
+        }
+        #endregion
+
+        protected void EvaluacionesTodasGrid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int seleccion = EvaluacionesTodasGrid.SelectedIndex;
+            EEvaluacionInstructor _evaluacion = _listaEvaluacion[seleccion];
+            Session["idEvaluacion"] = _evaluacion;
+
+            Response.Redirect("Modificar.aspx");
+        }
+
+        protected void EvaluacionesTodasGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            EvaluacionesTodasGrid.PageIndex = e.NewPageIndex;
+            llenarGridViewEvaluaciones();
+        }
+
     }
 }
