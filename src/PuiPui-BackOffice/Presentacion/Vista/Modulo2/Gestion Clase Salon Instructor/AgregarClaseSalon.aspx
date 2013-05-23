d23511cfe146e35561144e1efd684c4b5dbb97c7
CarlosCede?o <CarlosCede?o@localhost>
2013-05-23 07:38:16 -0530

    Crud Clase_salon

---
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion Clase Salon Instructor/AgregarClaseSalon.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/MasterPage/Site.Master" 
+    AutoEventWireup="true" CodeBehind="AgregarClaseSalon.aspx.cs" 
+    Inherits="PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_Clase_Salon_Instructor.AgregarClaseSalon" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="superior">
+    <h2 style="margin-left:350px">asignar clase Salon</h2>
+    
+    </div>
+
+    <div style="height:30px; text-align:center; font-family:Verdana;font-size: 1.5em;">
+        <asp:Label ID="falla" runat="server" Text="Operación Fallida" CssClass="falla" 
+            Visible="False" ForeColor="Red"></asp:Label>
+        <asp:Label ID="Exito" runat="server" Text="Operación Exitosa" CssClass="Exito" 
+            Visible="False"></asp:Label>
+    </div>
+
+    <div  >
+        <fieldset style="width:400px; height:180px; margin-left:23%;">
+               
+            <table style="margin:5% auto auto 8%; height: 140px; width: 476px;" border="0" cellspacing="0" cellpadding="0" >
+            <tr>
+                <td class="auto-style3">
+                    <asp:Label ID="Label4" runat="server" Text="Label">Nombre de la clase:</asp:Label>
+                </td>
+                <td class="auto-style4" align="left">
+                    <asp:DropDownList ID="ComboClase" runat="server" Height="18px" Width="120px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Label ID="Label3" runat="server" Text="Label">Ubicacion del Salon:</asp:Label>
+                </td>
+                <td class="auto-style2" colspan="0" align="left">
+                    <asp:DropDownList ID="ComboSalon" runat="server" Height="18px" Width="120px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style5">
+                    <asp:Label ID="Label5" runat="server" Text="Label">Instructor:</asp:Label>
+                    </td>
+                <td class="auto-style6" colspan="0">
+                    <asp:DropDownList ID="ComboInstructor" runat="server" Height="18px" Width="120px">
+                    </asp:DropDownList>
+                    </td>
+            </tr>
+            <tr >
+                <td colspan="2"  style="text-align:center">
+                <asp:Button ID="defaultButton" runat="server" Text="Aceptar" CssClass="button" OnClick="defaultButton_Click" />
+                </td>
+            </tr>
+            </table>          
+        </fieldset>
+    </div>
+</asp:Content>
+
+<asp:Content ID="Content3" runat="server" contentplaceholderid="HeadContent">
+    <style type="text/css">
+        .auto-style1 {
+            width: 127px;
+        }
+        .auto-style2 {
+            width: 246px;
+        }
+        .auto-style3 {
+            width: 127px;
+            height: 34px;
+        }
+        .auto-style4 {
+            width: 246px;
+            height: 34px;
+        }
+        .auto-style5 {
+            width: 127px;
+            height: 23px;
+        }
+        .auto-style6 {
+            width: 246px;
+            height: 23px;
+        }
+        #TextArea1 {
+            height: 71px;
+        }
+    </style>
+</asp:Content>
+
+
---
+++ src/PuiPui-BackOffice/Presentacion/Vista/Modulo2/Gestion Clase Salon Instructor/AgregarClaseSalon.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using PuiPui_BackOffice.Entidades.Clase;
+using PuiPui_BackOffice.Entidades.Salon;
+using PuiPui_BackOffice.Entidades.Instructor;
+using PuiPui_BackOffice.LogicaDeNegocios.LogicaClase;
+using PuiPui_BackOffice.LogicaDeNegocios.LogicaSalon;
+using PuiPui_BackOffice.LogicaDeNegocios.Instructor;
+
+namespace PuiPui_BackOffice.Presentacion.Vista.Modulo2.Gestion_Clase_Salon_Instructor
+{
+    public partial class AgregarClaseSalon : System.Web.UI.Page
+    {
+        List<Clase> _listClase = new List<Clase>();
+        List<Salon> _listSalon = new List<Salon>();
+        List<Instructor> _listInstructor = new List<Instructor>();
+        List<ClaseSalon> _listClaseSalon = new List<ClaseSalon>();
+        LogicaClase _accesoClase = new LogicaClase();
+        LogicaSalon _accesosSalon = new LogicaSalon();
+        LogicaInstructor _accesoInstructor = new LogicaInstructor();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LlenarCombos();
+
+        }
+        private void Init()
+        {
+            _listClase = new List<Clase>();
+            _listSalon = new List<Salon>();
+            _listInstructor = new List<Instructor>();
+            _listClaseSalon = new List<ClaseSalon>();
+            _accesoClase = new LogicaClase();
+            _accesosSalon = new LogicaSalon();
+            _accesoInstructor = new LogicaInstructor();
+        }
+        private void LlenarCombos()
+        {
+            _listClase = _accesoClase.ObtenerClases();
+            _listSalon = _accesosSalon.ObtenerSalones();
+            _listInstructor = _accesoInstructor.ConsultarTodosInstructores();
+
+            foreach (Clase clas in _listClase)
+            {
+                ComboClase.Items.Add(clas.Nombre);
+            }
+            foreach (Salon sal in _listSalon)
+            {
+                ComboSalon.Items.Add(sal.Ubicacion);
+            }
+            foreach (Instructor ins in _listInstructor)
+            {
+                ComboInstructor.Items.Add(ins.NombrePersona1+" "+ins.ApellidoPersona1);
+            }
+
+        }
+
+        protected void defaultButton_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            int idc, ids, idi;
+
+            foreach (ClaseSalon item in _listClaseSalon)
+            {
+                if (ComboClase.SelectedItem.ToString().Equals(item.Clase.Nombre))
+                {
+                    flag = true;
+                    if (ComboSalon.SelectedItem.ToString().Equals(item.Salon.Ubicacion))
+                    {
+                        flag = true;
+                        if (ComboInstructor.SelectedItem.ToString().Equals(item.Instructor.NombrePersona1 + " " + item.Instructor.ApellidoPersona1))
+                        {
+                            flag = true;
+                        }
+                        else
+                        {
+                            flag = false;
+                        }
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag == true)
+                {
+                    falla.Text = "Ya se encuentra registrada esta opcion";
+                    falla.Visible = true;
+                }
+                else
+                {
+
+                    flag = _accesosSalon.AgregarSalonesClase(_listClase[ComboClase.SelectedIndex].IdClase, _listInstructor[ComboInstructor.SelectedIndex].IdPersona, _listSalon[ComboSalon.SelectedIndex].IdSalon);
+                    if (flag == true)
+                    {
+                        Exito.Visible = true;
+                    }
+                    else
+                    {
+                        falla.Visible = true;
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
