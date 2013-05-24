62bc9e64b8becb7995cd72a7502ce0fa5efee083
jamal <jamal@localhost>
2013-05-24 06:35:04 +0200

    Adicion de Clases para el manejo de eventos en calendario. Modificacion en master page para solo para colocarle el nombre delmodulo.

---
+++ src/PuiPui-FrontOffice/Entidades/ReservarClase/ReservacionClase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuiPui_FrontOffice.Entidades.ReservarClase
+{
+    public class ReservacionClase
+    {
+        #region atributos
+
+        private DateTime fechaReservacion;
+        private String statusReservacion;
+        private int horarioReserva;
+        private int idCliente;
+
+        #endregion
+
+        #region getter setter
+
+
+        public DateTime FechaReservacion
+        {
+            get { return fechaReservacion; }
+            set { fechaReservacion = value; }
+        }
+
+        public String StatusReservacion
+        {
+            get { return statusReservacion; }
+            set { statusReservacion = value; }
+        }
+
+        public int HorarioReserva
+        {
+            get { return horarioReserva; }
+            set { horarioReserva = value; }
+        }
+
+
+        public int IdCliente
+        {
+            get { return idCliente; }
+            set { idCliente = value; }
+        }
+
+        #endregion
+
+        #region constructor
+
+        public ReservacionClase()
+        { 
+        }
+        #endregion
+    }
+}
