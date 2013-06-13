dcf7a9a6a68986e149f2d55dc9bd59d4232b6c66
Jesus <Jesus@localhost>
2013-06-13 14:14:01 -0530

    Paso de entidades a la nueva capa logica

--- PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/Clase.cs
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/Clase.cs
@@ -8,5 +8,81 @@ namespace PuiPuiCapaLogicaDeNegocios.Entidades.EClases
 {
     public class Clase : AEntidad
     {
+        #region Atributos
+
+        private String _nombre;
+        private int _idClase;
+        private String _descripcion;
+        private int _status;
+         private DateTime _horario;
+        #endregion
+
+        #region Getter Setter
+
+        public int Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public String Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        public int IdClase
+        {
+            get { return _idClase; }
+            set { _idClase = value; }
+        }
+
+        public String Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        #endregion
+
+         #region Constructores
+
+        public Clase(String nombre, int idClase, String descripcion)
+        {
+            this._descripcion = descripcion;
+            this._nombre = nombre;
+            this._idClase = idClase;
+        }
+            public Clase(String nombre, int idClase, String descripcion,int st,DateTime horario)
+        {
+            this._status = st;
+            this._descripcion = descripcion;
+            this._nombre = nombre;
+            this._idClase = idClase;
+            this._horario=horario;
+        }
+
+
+        public Clase(String nombre, int idClase, String descripcion,int st)
+        {
+            this._status = st;
+            this._descripcion = descripcion;
+            this._nombre = nombre;
+            this._idClase = idClase;
+        }
+
+        public Clase()
+        {
+        }
+
+        public Clase(int id)
+        {
+            this._idClase = id;
+        }
+
+        #endregion
+    }
+
+
     }
 }
--- PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/Salon.cs
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/Salon.cs
@@ -11,5 +11,70 @@ namespace PuiPuiCapaLogicaDeNegocios.Entidades.EClases
     /// </summary>
     public class Salon : AEntidad
     {
+
+        #region Atributos
+
+        private int _idSalon;
+        private String _ubicacion;
+        private int _capacidad;
+        private int _status;
+
+        #endregion
+
+        #region Metodos GetterSetter
+        public int IdSalon
+        {
+            get { return _idSalon; }
+            set { _idSalon = value; }
+        }
+        public String Ubicacion
+        {
+            get { return _ubicacion; }
+            set { _ubicacion = value; }
+        }
+
+        public int Capacidad
+        {
+            get { return _capacidad; }
+            set { _capacidad = value; }
+        }
+        public int Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+        #endregion
+
+        #region Contructores
+
+        public Salon(int idSalon, String ubicacion, int capacidad)
+        {
+            this._capacidad = capacidad;
+            this._idSalon = idSalon;
+            this._ubicacion = ubicacion;
+
+        }
+        public Salon(int idSalon, String ubicacion, int capacidad, int st)
+        {
+            this._status = st;
+            this._capacidad = capacidad;
+            this._idSalon = idSalon;
+            this._ubicacion = ubicacion;
+
+        }
+        public Salon()
+        {
+
+        }
+        public Salon(int id)
+        {
+            this.IdSalon = id;
+        }
+        #endregion
+
+        #region Metodos
+
+
+        #endregion
     }
 }
