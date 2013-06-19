1e8b66abc05ebc2e22c5c7af9b9224ca6f731b14
Gustavo <Gustavo@localhost>
2013-06-19 01:18:15 -0530

    Agregando entidades ejercicio musculo y Apersona

--- PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EEjercicios/Musculo.cs
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EEjercicios/Musculo.cs
@@ -8,5 +8,54 @@ namespace PuiPuiCapaLogicaDeNegocios.Entidades.EEjercicios
 {
     public class Musculo : AEntidad
     {
+
+        #region Atributos
+
+        private string _nombreMusculo;
+
+        #endregion
+
+        #region MetodosGetSet
+
+        public string NombreMusculo
+        {
+            get { return _nombreMusculo; }
+            set { _nombreMusculo = value; }
+        }
+
+        public int IdMusculo
+        {
+            get { return _id; }
+            set { _id = value; }
+
+        }
+
+        #endregion MetodoGetSet
+
+        #region Constructores
+
+        public Musculo()
+        {
+
+        }
+
+        public Musculo(int id)
+        {
+            _id = id;
+        }
+
+        public Musculo(string nombre)
+        {
+            _nombreMusculo = nombre;
+        }
+
+        public Musculo(int id, string nombre)
+        {
+            _id = id;
+            _nombreMusculo = nombre;
+        }
+
+        #endregion Constructores
+
     }
 }
