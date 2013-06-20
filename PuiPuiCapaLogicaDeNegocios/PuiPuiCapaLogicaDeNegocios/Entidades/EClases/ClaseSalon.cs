cd7db50b7b9b327138029ea1aa71945a98d6004a
Jesus <Jesus@localhost>
2013-06-20 14:22:37 -0530

    Paso de entidades a la nueva capa logica

--- PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/ClaseSalon.cs
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Entidades/EClases/ClaseSalon.cs
@@ -4,18 +4,78 @@ using System.Linq;
 using System.Web;
 using PuiPuiCapaLogicaDeNegocios.Entidades;
 using PuiPuiCapaLogicaDeNegocios.Entidades.EEvaluaciones;
+using PuiPuiCapaLogicaDeNegocios.Entidades.EPersonas;
 using PuiPuiCapaLogicaDeNegocios.Entidades.EReservaciones;
 
 namespace PuiPuiCapaLogicaDeNegocios.Entidades.EClases
 {
     public class ClaseSalon : AEntidad, IEvaluable, IReservable
     {
-        public void evaluar(int calificacion)
+        #region Atributos
+
+        private int _id;
+        private Clase _clase;
+        private Salon _salon;
+        private Instructor.Instructor _instructor;
+        private int _disponibilidad;
+
+        #endregion
+
+        #region Getter Setter
+
+        public int Id
         {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public Clase Clase
+        {
+            get { return _clase; }
+            set { _clase = value; }
+        }
+        public Salon.Salon Salon
+        {
+            get { return _salon; }
+            set { _salon = value; }
+        }
+        public Instructor.Instructor Instructor
+        {
+            get { return _instructor; }
+            set { _instructor = value; }
+        }
+        public int Disponibilidad
+        {
+            get { return _disponibilidad; }
+            set { _disponibilidad = value; }
         }
 
-        public void reservar(EHorario.Horario horario)
+        #endregion
+
+        #region Constructor
+
+        public ClaseSalon()
         {
+            _clase = new Clase();
+            _salon = new Salon();
+            _instructor = new Instructor.Instructor();
         }
+        public ClaseSalon(Salon sal, Clase clas, Instructor.Instructor ins)
+        {
+            this.Instructor = ins;
+            this.Salon = sal;
+            this.Clase = clas;
+        }
+        public ClaseSalon(int iid, Salon sal, Clase clas, Instructor.Instructor ins, int disp)
+        {
+            this._id = iid;
+            this._instructor = ins;
+            this._salon = sal;
+            this._clase = clas;
+            this._disponibilidad = disp;
+        }
+        #endregion
+
+
+
     }
 }
