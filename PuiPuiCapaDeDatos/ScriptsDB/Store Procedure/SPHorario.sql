a48605b466099622fdee2af7ae413876bc2a18e9
Jesus <Jesus@localhost>
2013-06-22 14:16:01 -0530

    Store Procedures Clase,Salon,Clase Salon, Horario y fix Clase Salon

---
+++ PuiPuiCapaDeDatos/ScriptsDB/Store Procedure/SPHorario.sql
@@ -0,0 +1,19 @@
+﻿USE [puipuiDBv1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[agregarHorario]
+@hora_inicio time (7),
+@hora_fin time (7),
+@dia_semana int
+as
+
+BEGIN
+	INSERT
+	INTO Horario(hora_inicio,hora_fin,dia_semana)
+	VALUES (@hora_inicio,@hora_fin,@dia_semana);
+END;
+GO
+
