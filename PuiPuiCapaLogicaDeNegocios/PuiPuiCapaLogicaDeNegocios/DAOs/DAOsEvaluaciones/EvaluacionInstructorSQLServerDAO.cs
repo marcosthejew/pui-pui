47dafc5bc4cf2bd147e74d36258c6d4896c3ea73
Hernan <Hernan@localhost>
2013-06-23 01:31:05 -0530

    Se agrego el DAO, los atributos de la entidad, los comandos y la exepcion de evaluacion instructor

--- PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/DAOs/DAOsEvaluaciones/EvaluacionInstructorSQLServerDAO.cs
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/DAOs/DAOsEvaluaciones/EvaluacionInstructorSQLServerDAO.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using PuiPuiCapaLogicaDeNegocios.Excepciones;
 
 namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
 {
@@ -16,6 +17,11 @@ namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
     public class EvaluacionInstructorSQLServerDAO : ASQLServerDAO, 
                                                     IEvaluacionInstructorDAO
     {
+
+
+        #region Atributos
+        private List<Entidades.AEntidad> _listaEvaluacionInstructor;
+        #endregion
         /// <summary>
         /// Devuelve una lista con todas las entidades activas de 
         /// EvaluacionInstructor que se encuentran en la base de datos de SQL 
@@ -24,7 +30,64 @@ namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
         /// <returns></returns>
         public List<Entidades.AEntidad> ConsultarTodos()
         {
-            throw new NotImplementedException();
+
+
+            _listaEvaluacionInstructor = new List<Entidades.AEntidad>();
+            _listaEvaluacionInstructor.RemoveRange(0, _listaEvaluacionInstructor.Count);
+            try
+            {
+                obtenerConexion().Open();
+
+                SqlCommand cmd = new SqlCommand("[dbo].[ListarEvaluacionesInstructor]", obtenerConexion());
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Entidades.EEvaluaciones.EvaluacionInstructor evalua = (Entidades.EEvaluaciones.EvaluacionInstructor)Fabricas.FabricaEntidad.CrearEvaluacionInstructor();
+                    
+                    evalua.idEvaluacion = Convert.ToInt32(dr.GetValue(0));
+                    evalua.Fecha = Convert.ToDateTime(dr.GetValue(1));
+                    evalua.Calificacion = Convert.ToInt32(dr.GetValue(2));
+                    evalua.Inactivo = Convert.ToInt32(dr.GetValue(3));
+                    evalua.Observaciones = dr.GetString(4);
+                    
+                    evalua.idCliente = Convert.ToInt32(dr.GetValue(5));
+                    evalua.idInstructor = Convert.ToInt32(dr.GetValue(6));
+
+                    _listaEvaluacionInstructor.Add(evalua);
+
+                }
+                obtenerConexion().Close();
+            }
+            catch (ArgumentException e)
+            {
+                throw new ExecpcionEvaluacionInstructor("Parametros invalidos", e);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ExecpcionEvaluacionInstructor("Operacion Invalida", e);
+            }
+            catch (NullReferenceException e)
+            {
+                throw new ExecpcionEvaluacionInstructor("Objetos Vacios", e);
+            }
+            catch (SqlException e)
+            {
+                throw new ExecpcionEvaluacionInstructor("Error con la base de datos", e);
+            }
+            catch (Exception e)
+            {
+                throw new ExecpcionEvaluacionInstructor("Falla", e);
+            }
+            finally
+            {
+                obtenerConexion().Close();
+            }
+
+            return _listaEvaluacionInstructor;             
+
+
         }
 
         /// <summary>
@@ -37,8 +100,55 @@ namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
         /// </param>
         /// <returns></returns>
         public Entidades.AEntidad ConsultarPorId(int id)
-        {
-            throw new NotImplementedException();
+        {            
+            Entidades.EEvaluaciones.EvaluacionInstructor evalua = (Entidades.EEvaluaciones.EvaluacionInstructor)Fabricas.FabricaEntidad.CrearEvaluacionInstructor();
+            try
+            {
+                obtenerConexion().Open();
+                SqlCommand cmd = new SqlCommand("[dbo].[DetalleEvaluacionInstructor]", obtenerConexion());
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter param = new SqlParameter("@IdEvaluacionInstructor", id);
+                cmd.Parameters.Add(param);
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();                
+                while (dr.Read())
+                {                    
+                    evalua.idEvaluacion = Convert.ToInt32(dr.GetValue(0));
+                    evalua.Fecha = Convert.ToDateTime(dr.GetValue(1));
+                    evalua.Calificacion = Convert.ToInt32(dr.GetValue(2));
+                    evalua.Inactivo = Convert.ToInt32(dr.GetValue(3));
+                    evalua.Observaciones = dr.GetString(4);
+                    evalua.idCliente = Convert.ToInt32(dr.GetValue(5));
+                    evalua.idInstructor = Convert.ToInt32(dr.GetValue(6));
+                }
+               
+                obtenerConexion().Close();
+            }
+            catch (ArgumentException e)
+            {
+                throw new ExecpcionClaseSalon("Parametros invalidos", e);
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new ExecpcionClaseSalon("Operacion Invalida", e);
+            }
+            catch (NullReferenceException e)
+            {
+                throw new ExecpcionClaseSalon("Objetos Vacios", e);
+            }
+            catch (SqlException e)
+            {
+                throw new ExecpcionClaseSalon("Error con la base de datos", e);
+            }
+            catch (Exception e)
+            {
+                throw new ExecpcionClaseSalon("Falla", e);
+            }
+            finally
+            {
+                obtenerConexion().Close();
+            }
+            return evalua;
         }
 
         /// <summary>
@@ -51,7 +161,70 @@ namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
         /// <returns></returns>
         public int Agregar(Entidades.AEntidad entidad)
         {
-            throw new NotImplementedException();
+
+            int insercion = 1;
+            Entidades.EEvaluaciones.EvaluacionInstructor evaluacion = (Entidades.EEvaluaciones.EvaluacionInstructor)Fabricas.FabricaEntidad.CrearEvaluacionInstructor();
+            try
+            {
+                obtenerConexion().Open();
+                SqlCommand cmd = new SqlCommand("[dbo].[AgregarEvaluacionInstructor]", obtenerConexion());
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param = new SqlParameter("@fecha", evaluacion.Fecha);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@calificacion", evaluacion.Calificacion);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@inactivo", evaluacion.Inactivo);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@observaciones", evaluacion.Observaciones);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@id_cliente", evaluacion.idCliente);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@id_instructor", evaluacion.idInstructor);
+                cmd.Parameters.Add(param);
+
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+
+                insercion = 0;
+                obtenerConexion().Close();
+            }
+            catch (ArgumentException e)
+            {
+                insercion = 1;
+                throw new ExecpcionClaseSalon("Parametros invalidos", e);
+            }
+            catch (InvalidOperationException e)
+            {
+                insercion = 1;
+                throw new ExecpcionClaseSalon("Operacion Invalida", e);
+            }
+            catch (NullReferenceException e)
+            {
+                insercion = 1;
+                throw new ExecpcionClaseSalon("Objetos Vacios", e);
+            }
+            catch (SqlException e)
+            {
+                insercion = 1;
+                throw new ExecpcionClaseSalon("Error con la base de datos", e);
+            }
+            catch (Exception e)
+            {
+                insercion = 1;
+                throw new ExecpcionClaseSalon("Falla", e);
+            }
+            finally
+            {
+                obtenerConexion().Close();
+
+            }
+            return insercion;
         }
 
         /// <summary>
@@ -84,7 +257,76 @@ namespace PuiPuiCapaLogicaDeNegocios.DAOs.DAOsEvaluaciones
         /// <returns></returns>
         public bool Modificar(int id, Entidades.AEntidad entidad)
         {
-            throw new NotImplementedException();
+            Boolean insercion = false;
+            Entidades.EEvaluaciones.EvaluacionInstructor evaluacion = (Entidades.EEvaluaciones.EvaluacionInstructor)Fabricas.FabricaEntidad.CrearEvaluacionInstructor();
+            try
+            {
+                obtenerConexion().Open();
+                SqlCommand cmd = new SqlCommand("[dbo].[AgregarEvaluacionInstructor]", obtenerConexion());
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param = new SqlParameter("@idEvaluacion", evaluacion.idEvaluacion);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@fecha", evaluacion.Fecha);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@calificacion", evaluacion.Calificacion);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@inactivo", evaluacion.Inactivo);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@observaciones", evaluacion.Observaciones);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@id_cliente", evaluacion.idCliente);
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter("@id_instructor", evaluacion.idInstructor);
+                cmd.Parameters.Add(param);
+
+                SqlDataReader dr;
+                dr = cmd.ExecuteReader();
+
+                insercion = true;
+                obtenerConexion().Close();
+
+            }
+            catch (ArgumentException e)
+            {
+                insercion = false;
+                throw new ExecpcionClaseSalon("Parametros invalidos", e);
+            }
+            catch (InvalidOperationException e)
+            {
+                insercion = false;
+                throw new ExecpcionClaseSalon("Operacion Invalida", e);
+            }
+            catch (NullReferenceException e)
+            {
+                insercion = false;
+                throw new ExecpcionClaseSalon("Objetos Vacios", e);
+            }
+            catch (SqlException e)
+            {
+                insercion = false;
+                throw new ExecpcionClaseSalon("Error con la base de datos", e);
+            }
+            catch (Exception e)
+            {
+                insercion = false;
+                throw new ExecpcionClaseSalon("Falla", e);
+            }
+            finally
+            {
+
+                obtenerConexion().Close();
+
+
+            }
+            return insercion;
+
         }
     }
 }
---
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Excepciones/ExecpcionEvaluacionInstructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuiPuiCapaLogicaDeNegocios.Excepciones
+{
+    public class ExecpcionEvaluacionInstructor: AExcepcionLogicaPuiPui
+    {
+        
+        public ExecpcionEvaluacionInstructor(string message) : base(message) { }
+        public ExecpcionEvaluacionInstructor(string message, Exception inner) : base(message, inner) { }
+    }
+}
