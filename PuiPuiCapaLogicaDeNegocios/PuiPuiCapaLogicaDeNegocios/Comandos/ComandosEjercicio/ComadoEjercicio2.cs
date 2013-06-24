81a07d6b4909d2e964f91e0f8ad8be903d81f347
CarlosCede?o <CarlosCede?o@localhost>
2013-06-24 17:46:20 -0530

    subiendo archivo ComandoEjercicio2

---
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Comandos/ComandosEjercicio/ComadoEjercicio2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PuiPuiCapaLogicaDeNegocios.Comandos.ComandosEjercicio
+{
+    public class ComadoEjercicio2
+    {
+    }
+}
