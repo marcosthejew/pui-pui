7d9f3aa05bb4c828d9d7f59e0471b2f0c47b7fcd
Yeimy <Yeimy@localhost>
2013-06-26 15:17:12 -0530

    Agregando comandos de Musculo

---
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Comandos/ComandoMusculo/ComandoConsultarTodosLosMusculos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PuiPuiCapaLogicaDeNegocios.Entidades;
+using PuiPuiCapaLogicaDeNegocios.Excepciones;
+using PuiPuiCapaLogicaDeNegocios.Fabricas;
+
+namespace PuiPuiCapaLogicaDeNegocios.Comandos.ComandoMusculo
+{
+    public class ComandoConsultarTodosLosMusculos: AComando<List<AEntidad>>
+    {
+        
+        public override List<AEntidad> Ejecutar()
+        {
+            List<AEntidad> listaConsulta;
+            try
+            {
+                listaConsulta = FabricaSQLServerDAO.obtenerInstancia().CrearMusculoSQLServerDAO().ConsultarTodos();
+            }
+            catch (ExcepcionMusculoConexionBD e)
+            {
+                throw new ExcepcionMusculoConexionBD("No se pudo consultar los musculos: " + e.Message, e);
+            }
+            return listaConsulta;
+        }        
+        
+    }
+}
