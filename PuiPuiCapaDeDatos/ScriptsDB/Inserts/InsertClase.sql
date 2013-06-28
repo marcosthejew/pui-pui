d4652d99f69f02593d7aceeb0ba90a151cd8d615
Jesus <Jesus@localhost>
2013-06-28 15:31:54 -0530

    Fix Insert Clase sql

--- PuiPuiCapaDeDatos/ScriptsDB/Inserts/InsertClase.sql
+++ PuiPuiCapaDeDatos/ScriptsDB/Inserts/InsertClase.sql
@@ -1,7 +1,10 @@
-ï»¿USE [puipuiDBv1]
-
-Insert into Clase values ('Clase23','Spining Class',0);
-Insert into Clase values ('Clase12','Abs Class',0);
-Insert into Clase values ('Clase55','Kickboxing Class',0);
-Insert into Clase values ('Clase88','Aerobics Class',0);
-Insert into Clase values ('Clase44','Yoga Class',0);
+INSERT INTO Clase VALUES ('Spinning','Es un ejercicio aeróbico con bicicletas',1 );
+INSERT INTO Clase VALUES ('DragonFight',' Es un sistema de entrenamiento diseñado con la base del karate contemporáneo',1 );
+INSERT INTO Clase VALUES ('TaeBo','Es una rutina de ejercicios principalmente asociados al boxeo y al Taekwondo',1 );
+INSERT INTO Clase VALUES ('Bailoterapia','Ejercicio al ritmo de la música que permite a los participantes divertirse y tonificarse',1 );
+INSERT INTO Clase VALUES ('Abdominales','Ejercicio para tonificar el área media',1 );
+INSERT INTO Clase VALUES ('Fit Combat','Rutina de fitness que involucra el combate cuerpo a cuerpo',1 );
+INSERT INTO Clase VALUES ('Glutab','Ejercicio para las chicas donde se ejercitan los gluteos y el abdomen simultaneamente',1 );
+INSERT INTO Clase VALUES ('TRX','Ejercicio con cuerdas para tonificar los musculos',1 );
+INSERT INTO Clase VALUES ('Pilates','Se centra en el desarrollo de los músculos internos para mantener el equilibrio corporal',1 );
+INSERT INTO Clase VALUES ('Yoga','Ejercicio que permite estirar el cuerpo y lograr paz interna',1 );
