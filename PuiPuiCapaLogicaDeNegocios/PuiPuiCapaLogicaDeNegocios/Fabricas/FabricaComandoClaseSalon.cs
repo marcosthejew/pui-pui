5bb97623d18cffced1a1da9d579bc8d36895aa75
Jesus <Jesus@localhost>
2013-06-27 22:34:33 -0530

    Algo que falto de Clase Salon

---
+++ PuiPuiCapaLogicaDeNegocios/PuiPuiCapaLogicaDeNegocios/Fabricas/FabricaComandoClaseSalon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PuiPuiCapaLogicaDeNegocios.Comandos;
+using PuiPuiCapaLogicaDeNegocios.Entidades.EClases;
+using PuiPuiCapaLogicaDeNegocios.Comandos.ComandasoClaseSalon;
+
+namespace PuiPuiCapaLogicaDeNegocios.Fabricas
+{
+    public class FabricaComandoClaseSalon
+    {
+        public static AComando<List<ClaseSalon>>
+        CrearComandoConsultarClaseSalonTodos()
+        {
+
+            return new ComandoConsultarClaseSalonTodos();
+        }
+        public static AComando<List<ClaseSalon>>
+        CrearComandoBusquedaClase(string clase)
+        {
+
+            return new ComandoBusquedaClase(clase);
+        
+        }
+
+        public static AComando<List<ClaseSalon>>
+        CrearComandoBusquedaSalon(string salon)
+        {
+            return new ComandoBusquedaSalon(salon);
+        }
+
+
+        public static AComando<List<ClaseSalon>>
+        CrearComandoBusquedaInstructor(string instructor)
+        {
+
+            return new ComandoBusquedaInstructor(instructor);
+        }
+        public static AComando<int>
+        CrearComandoInsertarClaseSalon(ClaseSalon claseSalon)
+        {
+
+            return new ComandoAgregarClaseSalon(claseSalon);
+        }
+       public static AComando<string>
+        CrearComandoSerializar(List<ClaseSalon> lista)
+       {
+
+           return new ComandoSerializarClaseSalon(lista);
+       }
+
+
+
+    }
+}
