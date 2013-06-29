2e39c95bd09689dfa5b6dacb7f1673865ca55446
Angie <Angie@WIN-FGJUA6E352T.localdomain>
2013-06-28 23:01:01 -0530

    Stored Procedure e Insert  Rutina, Historial_Ejercicio

---
+++ PuiPuiCapaDeDatos/ScriptsDB/Inserts/InsertsRutina.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO Rutina (nombre, descripcion, inactivo) VALUES ('Abdo-Resistencia', 'Rutina para tonificar abdominales' , 1 );
+INSERT INTO Rutina (nombre, descripcion, inactivo) VALUES ('Brazitos', 'Rutina para tonificar los brazos' , 1 );
+INSERT INTO Rutina (nombre, descripcion, inactivo) VALUES ('Adelgaza', 'Rutina para mejorar condicion fisica' , 1 );
+INSERT INTO Rutina (nombre, descripcion, inactivo) VALUES ('Leg-Sculpture', 'Rutina para tonificar las piernas' , 1 );
+INSERT INTO Rutina (nombre, descripcion, inactivo) VALUES ('Super-Shaper', 'Rutina para tonificar varias partes del cuerpo' , 1 );
+
+
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (20, 0, 7,1,1,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (0, 20, 7,1,2,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (20, 0, 7,1,3,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (0, 20, 7,2,4,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (20, 0, 7,2,5,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (0, 20, 8,3,6,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (20, 0, 8,3,7,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (0, 20, 8,1,8,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (20, 0, 8,1,9,0);
+insert into Historial_Ejercicio (duracion_minutos,repeticiones,id_cliente,id_rutina, id_ejercicio, inactivo) values (0, 20, 8,3,1,0);
---
+++ PuiPuiCapaDeDatos/ScriptsDB/Store Procedure/SPRutina.sql
@@ -0,0 +1,184 @@
+﻿USE [puipuiDBv1]
+GO
+/*INSERTAR RUTINA */
+USE [puipuiDBv1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[insertar_Rutina]
+@nombre nchar(50),
+@descripcion nchar(50)
+
+as
+
+BEGIN
+	INSERT INTO Rutina VALUES (@nombre,@descripcion,0);	
+END;
+
+GO
+
+
+USE [puipuiDBv1]
+GO
+/****** Object:  StoredProcedure [dbo].[consultarRutinasPorCliente]    Script Date: 23/06/2013 16:55:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[consultarRutinasPorCliente]
+	@idPersona bigint
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+    SELECT distinct(R.id_rutina), r.nombre, r.descripcion, H.inactivo
+	FROM Rutina R, Historial_Ejercicio H 
+	WHERE H.id_cliente = @idPersona AND h.id_rutina = r.id_rutina
+	group by R.id_rutina, r.nombre, r.descripcion, H.inactivo
+	
+END;
+
+GO
+use [puipuiDBv1]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE ConsultarEjerciciosPorIDRutina
+	@idRutina int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT e.id_ejercicio, e.nombre, e.descripcion, h.duracion_minutos, h.repeticiones
+	FROM Rutina r, Historial_Ejercicio h, Ejercicio e
+	WHERE r.id_rutina = h.id_rutina and h.id_ejercicio= e.id_ejercicio and r.id_rutina = @idRutina
+
+END;
+GO
+USE [puipuiDBv1]
+GO
+/****** Object:  StoredProcedure [dbo].[ActivarDesactivarRutina]    Script Date: 23/06/2013 16:22:53 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ActivarDesactivarRutina]
+	@idRutina int, 
+	@inactivo bit
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	UPDATE Historial_Ejercicio
+	SET 
+		inactivo = @inactivo
+		
+	WHERE id_rutina = @idRutina
+END;
+GO
+
+/*****************Stored Procedures de Ejercicios ******************************/
+
+/*CONSULTAR LISTA DE EJERCICIOS */
+USE [puipuiDBv1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON 
+GO
+CREATE procedure [dbo].[consultarTodosEjerciciosR]
+as
+BEGIN
+set nocount on	
+        select E.id_ejercicio, E.nombre
+        from Ejercicio E
+        order by E.nombre ASC
+END;
+GO
+
+/*stored insertar historial ejercicio*/
+USE [puipuiDBv1]
+GO
+/****** Object:  StoredProcedure [dbo].[insertar_Rutina]    Script Date: 18/06/2013 14:42:30 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE  procedure [dbo].[insertar_HistorialEjercicio]
+@duracion int,
+@repeticion int,
+@idCliente int,
+@idRutina int,
+@idEjercicio int,
+@inactivo bit
+as
+
+BEGIN
+	INSERT INTO Historial_Ejercicio VALUES (@duracion, @repeticion, @idCliente, @idRutina, @idEjercicio, @inactivo);	
+END;
+
+GO
+--ultimo id rutina
+
+/*metodo para buscar el ultimo id insertado */
+USE [puipuiDBv1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[buscarUltimoIdRutina]
+as
+BEGIN
+	set nocount on
+		
+	SELECT max(id_rutina)
+	FROM Rutina 	
+END;
+GO
+
+
+USE [puipuiDBv1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[consultarStatusRutinaPorID]
+	@idRutina  int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT distinct(h.inactivo)
+	FROM Historial_Ejercicio h
+	WHERE h.id_rutina =  @idRutina
+	group by h.inactivo
+
+END
+GO
+
+
+USE [puipuiDBv1]
+GO
+/****** Object:  StoredProcedure [dbo].[consultarPersonaPorLogin]    Script Date: 28/06/2013 23:49:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[consultarPersonaPorLogin]
+@loginPersona nchar (50)
+as
+
+BEGIN
+ set nocount on
+  
+  SELECT *
+ FROM Persona AS P
+ where P.login = @loginPersona
+
+END;
