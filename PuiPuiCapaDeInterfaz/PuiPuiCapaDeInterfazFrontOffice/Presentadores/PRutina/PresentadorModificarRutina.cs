ab1da41f8b22ae672fd51d5f01e69541e474988b
Patricia <Patricia@localhost>
2013-06-28 22:51:59 -0530

    agregando griviews de modificar y consultar

---
+++ PuiPuiCapaDeInterfaz/PuiPuiCapaDeInterfazFrontOffice/Presentadores/PRutina/PresentadorModificarRutina.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using PuiPuiCapaDeInterfazFrontOffice.Comandos;
+using PuiPuiCapaDeInterfazFrontOffice.Contratos.CTRutina;
+
+namespace PuiPuiCapaDeInterfazFrontOffice.Presentadores.PRutina
+{
+    public class PresentadorModificarRutina
+    {
+        #region Atributos
+
+        private IContratoBuscarRutina _vista;
+        private AComando<string> _comando1;
+        private AComando<DataTable> _comando2;
+        
+        #endregion
+
+        #region Constructor
+
+        public PresentadorModificarRutina(IContratoBuscarRutina vista)
+        {
+            this._vista = vista;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public void PintarConsultaRutina(int _idCliente)
+        {
+            _comando1 = FabricaComando.CrearComandoConsultarRutina(_idCliente);
+            string resultadoComando = "";
+            resultadoComando = _comando1.Ejecutar();
+
+            DataTable _tabla = new DataTable();
+            _comando2 = FabricaComando.CrearComandoDesSerializarRutina(resultadoComando);
+            _tabla = _comando2.Ejecutar();
+
+            _vista.GridConsultar1.DataSource = _tabla;
+            _vista.GridConsultar1.DataBind();
+
+        }
+        #endregion 
+
+    }
+}
