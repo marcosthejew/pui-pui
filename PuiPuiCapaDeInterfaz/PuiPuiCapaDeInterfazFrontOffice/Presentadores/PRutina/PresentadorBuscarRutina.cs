ee7660e97f4adefad09d9d6ef1ddc8882bebeb7c
Patricia <Patricia@localhost>
2013-06-28 21:14:03 -0530

    buscar con griview

---
+++ PuiPuiCapaDeInterfaz/PuiPuiCapaDeInterfazFrontOffice/Presentadores/PRutina/PresentadorBuscarRutina.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using PuiPuiCapaDeInterfazFrontOffice.Contratos.CTRutina;
+using PuiPuiCapaDeInterfazFrontOffice.Comandos;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+namespace PuiPuiCapaDeInterfazFrontOffice.Presentadores.PRutina
+{
+    public class PresentadorBuscarRutina
+    {
+        #region Atributos
+
+        private IContratoBuscarRutina _vista;
+        private AComando<string> _comando1;
+        private AComando<DataTable> _comando2;
+        
+        #endregion
+
+        #region Constructor
+
+        public PresentadorBuscarRutina(IContratoBuscarRutina vista)
+        {
+            this._vista = vista;
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public void PintarConsultaRutina(int _idCliente)
+        {
+            _comando1 = FabricaComando.CrearComandoConsultarRutina(_idCliente);
+            string resultadoComando = "";
+            resultadoComando = _comando1.Ejecutar();
+
+            DataTable _tabla = new DataTable();
+            _comando2 = FabricaComando.CrearComandoDesSerializarRutina(resultadoComando);
+            _tabla = _comando2.Ejecutar();
+
+            _vista.GridConsultar1.DataSource = _tabla;
+            _vista.GridConsultar1.DataBind();
+
+        }
+        #endregion 
+
+    }
+}
---
+++ PuiPuiCapaDeInterfaz/PuiPuiCapaDeInterfazFrontOffice/Vistas/VRutina/BuscarConsultar.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PuiPuiCapaDeInterfazFrontOffice.Contratos.CTRutina;
+using PuiPuiCapaDeInterfazFrontOffice.Presentadores.PRutina;
+
+namespace PuiPuiCapaDeInterfazFrontOffice.Vistas.VRutina
+{
+    public partial class BuscarConsultar : System.Web.UI.Page, IContratoBuscarRutina
+    {
+         #region Atributos
+
+        private PresentadorBuscarRutina _presentador;
+
+        #endregion
+
+        #region Constructor
+
+        public BuscarConsultar()
+        {
+            _presentador = new PresentadorBuscarRutina(this);
+        }
+
+        #endregion
+
+        #region Contrato
+
+        public GridView GridConsultar1
+        {
+            get { return GridConsultar; }
+            set { GridConsultar = value; }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                int _idCliente;
+                _idCliente = 7;
+                _presentador.PintarConsultaRutina(_idCliente);
+
+        }
+        protected void GridConsultar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idRutina = GridConsultar.SelectedIndex+1;
+            Session["idRutina"] = GridConsultar.SelectedIndex;
+            Response.Redirect("ConsultarRutina.aspx");
+            
+        }
+
+        protected void GridConsultar_RowCommand(Object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+
+        protected void GridConsultar_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+
+        }
+        public void Redireccionar(string _ruta)
+        {
+            Response.Redirect(_ruta);
+        }
+        #endregion
+    }
+}
